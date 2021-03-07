a6b5756ebdb57c1d8fb1060e577068036a5450f0
Luke Orellana <36606066+lukeorellana@users.noreply.github.com>
2021-03-07 17:29:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 module "server" {
-  source = "./modules/terraform-azure-wsserver"
+  source = "./modules/terraform-azure-server"
 
   subnet_id = azurerm_subnet.subnet.id
   resource_group_name = azurerm_resource_group.rg.name
